20b327ed49caf4f9ae45feade4a459659a48bf88
YanjunYu-1 <y9y9j8@163.com>
2022-05-22 19:25:30 -0500

    添加项目文件。

---
+++ FinalByMyself_0522/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalByMyself_0522.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
